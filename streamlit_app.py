474068d55bba8adf85ece056447a55410ce5817f
Lavkush Gupta <lavkushgupt@live.in>
2022-02-02 15:23:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import tensorflow
-import imutils
+#import imutils
 import cv2
 from tensorflow.keras.models import load_model
-from imutils.contours import sort_contours
+#from imutils.contours import sort_contours
 import numpy as np
 import argparse
 
