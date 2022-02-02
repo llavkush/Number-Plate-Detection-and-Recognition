6f6c2472f314c2804a02bb1baf8fe5625ef69af6
Lavkush Gupta <lavkushgupt@live.in>
2022-02-02 15:17:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import tensorflow
+import imutils
+import cv2
 from tensorflow.keras.models import load_model
-from imutils import sort_contours
+from imutils.contours import sort_contours
 import numpy as np
 import argparse
-import imutils
-#import cv2
 
 loaded_model = load_model('/content/OCR_Resnet.h5')
 
